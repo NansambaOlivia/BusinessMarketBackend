b141c2f4cb199c89e0c148a992c3146e00e0fa6e
NANSAMBA-GIT <lwangaolivia20@gmail.com>
2026-01-09 20:39:51 +0300

    created the urls and views of the product app

--- marketplace/commerce/urls.py
+++ marketplace/commerce/urls.py
@@ -39,6 +39,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
+    path('api/products/', include('products.urls')),
     
     # JWT Authentication
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
--- marketplace/products/views.py
+++ marketplace/products/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Product
+from .serializers import ProductSerializer
+from .permissions import CanApproveProduct
 
-# Create your views here.
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [permissions.IsAuthenticated, CanApproveProduct]
+
+    def get_queryset(self):
+        user = self.request.user
+
+        if user.is_authenticated:
+            return Product.objects.filter(business=user.business)
+
+        return Product.objects.filter(status='approved')
+
+    def perform_create(self, serializer):
+        serializer.save(
+            created_by=self.request.user,
+            business=self.request.user.business
+        )
