d7da484cfc09f542cc535853d2c38defe129bd9e
NANSAMBA-GIT <lwangaolivia20@gmail.com>
2026-01-09 21:39:14 +0300

    added the products to the admin interface

--- marketplace/products/admin.py
+++ marketplace/products/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'price',
+        'status',
+        'business',
+        'created_by',
+        'created_at',
+    )
+    list_filter = ('status', 'business')
+    search_fields = ('name', 'description')
